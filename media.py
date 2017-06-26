61ec9d358af25efc78da0e1499f59e980dcb53bc
santiagoxrestrepo <santiagoxrestrepo@gmail.com>
2017-06-25 23:14:39 -0500

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+
 class Movie(object):
 
     """This class was created to hold the movie information which includes
@@ -9,17 +10,19 @@ class Movie(object):
         -Youtube Trailer
     """
 
-
-# This creates the different variables
-
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):    
+    """
+    This creates the different variables
+    """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
-
-# This playes the movie trailer in the browser        
+        self.trailer_youtube_url = trailer_youtube        
 
     def show_trailer(self):
+    """
+    This playes the movie trailer in the browser
+    """
         webbrowser.open(self.trailer_youtube_url)
       
